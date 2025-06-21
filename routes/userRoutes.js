887811d081458e18cf54090f5b0957fa2d8d2a0c
Arindam Kumar <kumararindamAK.2002@gmail.com>
2025-06-22 02:47:15 +0530

    Integrated Razorpay test card payment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,20 @@
 import express from 'express'
-import {registerUser, loginUser, userCredits} from '../controllers/userController.js'
+import {registerUser, loginUser, userCredits,paymentRazorpay,verifyRazorpay} from '../controllers/userController.js'
 import userAuth from '../middlewares/auth.js'
 
 const userRouter=express.Router()
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
-userRouter.post('/credits',userAuth, userCredits)
+userRouter.get('/credits',userAuth, userCredits) 
+/* IMPORTANT !!
+If you are using post method with '/credits' endpoint,
+then use post method for making an axios request to the API
+in the AppContext file.
+*/
+
+userRouter.post('/pay-razor',userAuth, paymentRazorpay)
+userRouter.post('/verify-razor',verifyRazorpay)
 
 export default userRouter
 
